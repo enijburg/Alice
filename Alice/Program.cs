bcba51f2feab8e82c9f2a636d8226922f7af231d
Ewart Nijburg <enijburg@troolean.nl>
2019-11-12 13:46:25 +0100

    Article code

---
+++ Alice/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Alice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
